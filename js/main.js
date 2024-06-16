e37355f8e56a38fb7912bd76176dfa42d46a2c50
Gusev2020 <gusev12.gusev@yandex.ru>
2024-06-16 15:25:28 +0300

    Правка меню, якорные ссылки, ссылки на формы и пинтерест

--- js/main.js
+++ js/main.js
@@ -8,3 +8,19 @@ headerNav.addEventListener('click', function () {
   btnNav.classList.toggle('nav-icon-btn--active')
   headerInner.classList.toggle('header__inner--mobile')
 })
+
+// якоря
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', (event) => {
+    event.preventDefault()
+    const blockId = anchor.getAttribute('href')
+    document.querySelector('' + blockId).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    })
+  })
+  console.log(anchor)
+}
